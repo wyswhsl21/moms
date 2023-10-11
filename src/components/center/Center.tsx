534755ffab29c14c4c623dd5c2f59ef6f11269eb
unknown <wyswhsl21@naver.com>
2023-10-12 01:24:02 +0900

    mui 익히기

---
+++ src/components/center/Center.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Center.scss';
+const Center = () => {
+  const centerMenuList = [];
+  return (
+    <div className="Center">
+      <span>온맘터치</span>
+    </div>
+  );
+};
+
+export default Center;
